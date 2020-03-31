99f2666ddc31ad3a9eda5bfb4fe2875ea4411c9c
xy <exy2000a@163.com>
2020-03-31 16:05:18 +0800

    收货地址列表

---
+++ database/migrations/2020_03_31_072739_create_user_addresses_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_addresses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('province');
+            $table->string('city');
+            $table->string('district');
+            $table->string('address');
+            $table->unsignedInteger('zip');
+            $table->string('contact_name');
+            $table->string('contact_phone');
+            $table->dateTime('last_used_at')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_addresses');
+    }
+}
