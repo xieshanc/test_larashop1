d2ac6e2866198f4981ab9310a1b5801f415f1c8b
xy <exy2000a@163.com>
2020-03-31 15:14:18 +0800

    注册和登录

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -18,8 +18,24 @@
       <!-- Right Side Of Navbar -->
       <ul class="navbar-nav navbar-right">
         <!-- Authentication Links -->
-        <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
+        @guest
+        <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">登录</a></li>
+        <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">注册</a></li>
+        @else
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <img src="脏" class="img-responsive img-circle" width="30px" height="30px">
+            {{ Auth::user()->name }}
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" id="logout" href="#"
+               onclick="event.preventDefault();document.getElementById('logout-form').submit();">退出登录</a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              {{ csrf_field() }}
+            </form>
+          </div>
+        </li>
+        @endguest
       </ul>
     </div>
 
