038872647675c80c99cae1674cbb9e70527690a1
xy <exy2000a@163.com>
2020-04-02 11:37:28 +0800

    ‘提交订单’

---
+++ app/Http/Requests/OrderRequest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use App\Models\ProductSku;
+
+class OrderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'address_id' => [
+                'required',
+                Rule::exists('user_addresses', 'id')->where('user_id', $this->user()->id),
+            ],
+            'items' => ['required', 'array'],
+            'items.*.sku_id' => [
+                'required',
+                function ($attribute, $value, $fail) {
+                    echo $attribute . "<br>";
+                    if (!$sku = ProductSku::find($value)) {
+                        return $fail('商品不存在');
+                    }
+                    if (!$sku->product->on_sale) {
+                        return $fail('商品未上架');
+                    }
+                    if ($sku->stock === 0) {
+                        return $fail('商品已售完');
+                    }
+                    preg_match('/items\.(\d+)\.sku_id/', $attribute, $m);
+                    $index = $m[1];
+                    $amount = $this->input('items')[$index]['amount'];
+                    if ($amount > 0 && $amount > $sku->stock) {
+                        return $fail('库存不足');
+                    }
+                }
+            ], // END  items.*.sku_id
+            'items.*.amount' => ['required', 'integer', 'min:1'],
+        ];
+    }
+}
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -91,7 +91,7 @@ class Order extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function item()
+    public function items()
     {
         return $this->hasMany(OrderItem::class);
     }
