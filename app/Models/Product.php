c32800027a0c49bff8efa8ac976216e0a9673432
xy <exy2000a@163.com>
2020-04-07 20:18:32 +0800

    准本号

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,13 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    const TYPE_NORMAL = 'normal';
+    const TYPE_CROWDFUNDING = 'crowdfunding';
+
     protected $fillable = [
-        'title', 'description', 'image', 'on_sale', 'price',
+        'title', 'description', 'image', 'on_sale', 'price', 'type',
         // 'rating', 'sold_count', 'review_count',
     ];
 
     protected $casts = [
         'on_sale' => 'boolean',
+    ];
+
+    public static $typeMap = [
+        self::TYPE_NORMAL => '普通商品',
+        self::TYPE_CROWDFUNDING => '众筹商品',
     ];
 
     public function getImageUrlAttribute()
@@ -33,6 +41,11 @@ class Product extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function crowdfunding()
+    {
+        return $this->hasOne(CrowdfundingProduct::class);
     }
 
 }
